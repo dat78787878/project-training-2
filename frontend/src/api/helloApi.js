c19dfa77a88f9361ccc3e08f94ac52ae1b7daa63
unknown <you@example.com>
2022-03-01 11:52:53 +0700

    delete

--- frontend/src/api/helloApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient";
-class HelloApi {
-    getAll = (params) => {
-    const url = '/hello';
-    return axiosClient.get(url, { params });
-    };
-    }
-    const helloApi = new HelloApi();
-    export default helloApi;
