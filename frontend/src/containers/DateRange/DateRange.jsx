457ad44b06713a9dbf902abc1a3c1aca72dc02e8
unknown <you@example.com>
2022-03-18 09:18:44 +0700

    refs #5 fix display

--- frontend/src/containers/DateRange/DateRange.jsx
+++ frontend/src/containers/DateRange/DateRange.jsx
@@ -2,8 +2,8 @@ import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect, useCallback } from 'react';
 import { Button } from 'react-bootstrap';
 import { getData } from '../../redux/dateRange/actions';
 import moment from 'moment';
@@ -11,7 +11,6 @@ import { Link } from 'react-router-dom';
 
 const DateRange = () => {
   const dispatch = useDispatch();
-  const { fromDate, toDate } = useSelector((state) => state.dateRange);
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
   const [focusedInput, setFocusedInput] = useState(null);
@@ -24,22 +23,18 @@ const DateRange = () => {
     const params = Object.fromEntries(urlSearchParams.entries());
     setStartDate(moment(params.fromDate_));
     setEndDate(moment(params.toDate_));
-  });
+  }, [window.location.search]);
 
-  const handleClick = () => {
-    if (!!startDate && !!endDate) {
-      const start = moment(startDate).format('DD/MM/YYYY');
-      const end = moment(endDate).format('DD/MM/YYYY');
-      if (start !== fromDate || end !== toDate) {
-        dispatch(
-          getData({
-            fromDate: start,
-            toDate: end
-          })
-        );
-      }
-    }
-  };
+  const handleClick = useCallback(() => {
+    const start = moment(startDate).format('DD/MM/YYYY');
+    const end = moment(endDate).format('DD/MM/YYYY');
+    dispatch(
+      getData({
+        fromDate: start,
+        toDate: end
+      })
+    );
+  }, [startDate, endDate]);
 
   return (
     <div className="dateRange d-flex">
@@ -52,8 +47,8 @@ const DateRange = () => {
         onFocusChange={(focusedInput) => setFocusedInput(focusedInput)}
       />
       <Link to={`?fromDate_=${startDate}&toDate_=${endDate}`}>
-        <Button variant="primary" onClick={handleClick}>
-          OK
+        <Button variant="success" onClick={handleClick} className="m-1">
+          Ấn vào đây đi chị Mike
         </Button>
       </Link>
     </div>
